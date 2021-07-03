e88213df6e3adea4498d4381a7ec086f9ad72006
saci zakaria <sacideveloppeur@gmail.com>
2021-07-03 22:06:15 +0200

    DesignAudioplayer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'musique.dart';
 
@@ -14,13 +16,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title:  'Coda Music'),
+      home: MyHomePage(title: 'Coda Music'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
@@ -30,17 +31,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<Musique> maListeDeMusiques = [
-    new Musique('Theme Swift', 'Codabee', 'assets/un.jpg', 'https://codabee.com/wp-content/uploads/2018/06/un.mp3'),
-    new Musique('Theme Flutter', 'Codabee', 'assets/deux.jpg', 'https://codabee.com/wp-content/uploads/2018/06/deux.mp3'),
+    new Musique('Theme Swift', 'Codabee', 'assets/un.jpg',
+        'https://codabee.com/wp-content/uploads/2018/06/un.mp3'),
+    new Musique('Theme Flutter', 'Codabee', 'assets/deux.jpg',
+        'https://codabee.com/wp-content/uploads/2018/06/deux.mp3'),
   ];
   late Musique maMusiqueActuelle;
+  double position = 0.0;
   @override
   void initState() {
     super.initState();
     maMusiqueActuelle = maListeDeMusiques[0];
-
   }
 
   @override
@@ -51,18 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.grey[850],
         title: Text(widget.title),
       ),
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[600],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-        new Card(
-          elevation: 9.0,
-          child: new Container(
-            width: MediaQuery.of(context).size.height / 2.5,
-            child: new Image.asset(maMusiqueActuelle.imagePath),
-          ),
-        ),
+            new Card(
+              elevation: 9.0,
+              child: new Container(
+                width: MediaQuery.of(context).size.height / 2.5,
+                child: new Image.asset(maMusiqueActuelle.imagePath),
+              ),
+            ),
             texteAvecStyle(maMusiqueActuelle.titre, 1.6),
             texteAvecStyle(maMusiqueActuelle.artiste, 1.3),
             new Row(
@@ -80,37 +82,46 @@ class _MyHomePageState extends State<MyHomePage> {
                 texteAvecStyle('0:22', 1.0),
               ],
             ),
+            new Slider(
+                value: position,
+                min: 0.0,
+                max: 30.0,
+                inactiveColor: Colors.white,
+                activeColor: Colors.red,
+                onChanged: (double d) {
+                  setState(() {
+                    position = d;
+                  });
+                })
           ],
         ),
       ),
-    // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   IconButton bouton(IconData icone, double taille, ActionMusic action) {
-   return new IconButton(
-     icon: new Icon(icone),
-     onPressed:  () {
-       switch (action){
-         case ActionMusic.play:
-           print(('play'));
-           break;
-         case ActionMusic.pause:
-           print(('pause();'));
-           break;
-         case ActionMusic.forward:
-           print(('forward();'));
-           break;
-         case ActionMusic.rewind:
-           print(('rewind();'));
-           break;
-      }
-
-   }
-   );
+    return new IconButton(
+        iconSize: taille,
+        color: Colors.white,
+        icon: new Icon(icone),
+        onPressed: () {
+          switch (action) {
+            case ActionMusic.play:
+              print(('play'));
+              break;
+            case ActionMusic.pause:
+              print(('pause();'));
+              break;
+            case ActionMusic.forward:
+              print(('forward();'));
+              break;
+            case ActionMusic.rewind:
+              print(('rewind();'));
+              break;
+          }
+        });
   }
-
-
 
   Text texteAvecStyle(String data, double scale) {
     return new Text(
@@ -118,17 +129,11 @@ class _MyHomePageState extends State<MyHomePage> {
       textScaleFactor: scale,
       textAlign: TextAlign.center,
       style: new TextStyle(
-          color: Colors.black87,
+          color: Colors.limeAccent,
           fontSize: 20.0,
-          fontStyle: FontStyle.italic
-      ),
+          fontStyle: FontStyle.italic),
     );
   }
+}
 
-}
-enum ActionMusic {
-  play,
-  pause,
-  rewind,
-  forward
-}
+enum ActionMusic { play, pause, rewind, forward }
--- lib/musique.dart
+++ lib/musique.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/foundation.dart';
 
 class Musique {
-
   String titre;
   String artiste;
   String imagePath;
   String urlSong;
 
-  Musique(
-      this.titre,
-      this.artiste,
-      this.imagePath,
-      this.urlSong
-      )
-  {
+  Musique(this.titre, this.artiste, this.imagePath, this.urlSong) {
     this.titre = titre;
     this.artiste = artiste;
     this.imagePath = imagePath;
     this.urlSong = urlSong;
   }
-
 }
